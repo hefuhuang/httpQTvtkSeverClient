5915ccdd494b93141997923f8cdb7393974fbc64
huang <1966423770@qq.com>
2018-01-03 09:55:57 +0800

    statement

--- NetUtil/NetUtil.cpp
+++ NetUtil/NetUtil.cpp
@@ -1,3 +1,14 @@
+/*
+* Author	: huanghefu (Opensource)
+* Project	: https://github.com/hefuhuang/httpQTvtkSeverClient
+* QQ     	: 1966423770
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 #include "NetUtil.h"
 
 NetUtil::NetUtil()
--- NetUtil/NetUtil.h
+++ NetUtil/NetUtil.h
@@ -1,9 +1,35 @@
 #pragma once
 
+/*
+* Author	: huanghefu (Opensource)
+* Project	: https://github.com/hefuhuang/httpQTvtkSeverClient
+* QQ     	: 1966423770
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#ifndef _NETUTIL_H_
+#define _NETUTIL_H_
+
+#include <QObject>
 #include "netutil_global.h"
 
 class NETUTIL_EXPORT NetUtil
 {
 public:
-    NetUtil();
+    NetUtil(); 
+	~NetUtil();
+	 
+
+
+
 };
+
+
+
+
+#endif 
